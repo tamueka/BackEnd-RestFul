faa70999da68aab1d9b7e34b4692a9ec99fe06b9
tamueka <tamueka@hotmail.com>
2020-05-06 18:00:11 +0200

    validate entered data whit validator.js

--- controllers/article.js
+++ controllers/article.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const controller = {
+const validator = require("validator");
+const Article = require("../models/article");
 
+const controller = {
   datosCurso: (req, res) => {
     let hola = req.body.hola;
 
@@ -19,6 +21,38 @@ const controller = {
     });
   },
 
+  save: (req, res) => {
+    //Recoger parametros por post
+    var params = req.body;
+    //console.log(params);
+
+    //Validar datos (validator)
+    try {
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_content = !validator.isEmpty(params.content);
+    } catch {
+      return res.status(200).send({
+        message: "Faltan datos por enviar",
+      });
+    }
+
+    if (validate_title && validate_content) {
+      //Crear objeto a guardar
+
+      //Asignar valores
+
+      //Guardar el articulo
+
+      //Devolvemos respuesta
+      return res.status(200).send({
+        message: "Articulo guardado",
+      });
+    } else {
+      return res.status(200).send({
+        message: "Campos vacios",
+      });
+    }
+  },
 }; //end controller
 
 module.exports = controller;
--- routes/article.js
+++ routes/article.js
@@ -7,8 +7,12 @@ const ArticleController = require('../controllers/article');
 
 const router = express.Router();
 
+//Rutas de prueba
 router.post("/datos-curso", ArticleController.datosCurso);
 router.get('/test-de-controlador', ArticleController.test);
+
+//Rutas utiles
+router.post('/save', ArticleController.save);
 
 
 module.exports = router;
