36cfc1892c889c48272b7eaf9412a5fbee50f9f0
tamueka <tamueka@hotmail.com>
2020-05-07 16:58:58 +0200

    search methos in controller article whit article.find or

--- controllers/article.js
+++ controllers/article.js
@@ -282,10 +282,44 @@ const controller = {
       } else {
         return res.status(404).send({
           status: "error",
-          message: "La imagen no existe !!!"
+          message: "La imagen no existe !!!",
         });
       }
     });
+  },
+
+  search: (req, res) => {
+    //Sacar el string a buscar
+    var searchString = req.params.search;
+
+    //Find or + condiciones + ordenacion
+    Article.find({
+      $or: [
+        { title: { $regex: searchString, $options: "i" } },
+        { content: { $regex: searchString, $options: "i" } },
+      ],
+    })
+      .sort([["date", "descending"]])
+      .exec((err, articles) => {
+        if (err) {
+          return res.status(500).send({
+            status: "error",
+            message: "Error en la peticion",
+          });
+        }
+        if (!articles || articles.length <= 0) {
+          return res.status(404).send({
+            status: "error",
+            message: "Error no hay articulos para mostrar",
+          });
+        }
+
+        return res.status(200).send({
+          status: "success",
+          message: "Articulo encontrado !!!",
+          articles
+        });
+      });
   },
 }; //end controller
 
