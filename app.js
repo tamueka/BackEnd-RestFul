0ead15457aeb9a11c3666a4e2501dec1a1854367
tamueka <tamueka@hotmail.com>
2020-05-07 17:10:27 +0200

    configure cors access control allow (origin, headers, methods)

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ const article_routes = require('./routes/article')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// CORS
+// Configuramos CORS (Acceso Cruzado entre Dominios)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 
 // Añadir prefijos a las rutas / Cargar rutas
 app.use('/api', article_routes);
